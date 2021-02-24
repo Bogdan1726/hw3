b00f57a19b4e2d83f2686da55dfbb5b2d777eff3
Bohdan Rozhniatovskiy <bogdan24ro@gmail.com>
2021-02-24 12:34:43 +0200

    1-11

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+#1. Define the id of next variables:
 int_a = 55
 str_b = 'cursor'
 set_c = {1, 2, 3}
@@ -8,10 +9,30 @@ print(id(str_b))
 print(id(set_c))
 print(id(lst_d))
 print(id(dict_e))
+#2. Append 4 and 5 to the lst_d and define the id one more time.
 lst_d.append(4)
 lst_d.append(5)
 print(id(lst_d))
+#3. Define the type of each object from step 1.
 print(type(int_a), type(str_b), type(set_c), type(lst_d), type(dict_e))
+#4*. Check the type of the objects by using isinstance.
+print(isinstance(int_a, int), isinstance(str_b, str), isinstance(set_c, set), isinstance(lst_d, list), isinstance(dict_e, dict))
 
-print(isinstance(int_a, int), isinstance(str_b, str), isinstance(set_c,set), isinstance(lst_d, list), isinstance(dict_e, dict))
+#5. With .format and curly braces {}
+print("Anna has {} apples and {} peaches.".format(3, 5))
+#6. By passing index numbers into the curly braces.
+print("Anna has {1} apples and {0} peaches.".format(3, 5))
+#7. By using keyword arguments into the curly braces.
+print("Anna has {apple} apples and {peach} peaches.".format(apple=3, peach=5))
+#8*. With indicators of field size (5 chars for the first and 3 for the second)
+print("Anna has {0:5} apples and {1:3} peaches.".format(3, 5))
+#9. With f-strings and variables
+apple = 5
+peach = 7
+print(f"Anna has {apple} apples and {peach} peaches.")
+#10. With % operator
+print("Anna has %s apples and %s peaches." % (apple, peach))
+#11*. With variable substitutions by name (hint: by using dict)
+data_dict = {"a": apple, "b": peach}
+print("Anna has %(a)s apples and %(b)s peaches." % data_dict)
 
