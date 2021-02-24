8d65a560de63b673a408b2607a06b50d33a80da2
Bohdan Rozhniatovskiy <bogdan24ro@gmail.com>
2021-02-24 16:21:06 +0200

    1-25

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-#1. Define the id of next variables:
+""""#1. Define the id of next variables:
 int_a = 55
 str_b = 'cursor'
 set_c = {1, 2, 3}
@@ -74,3 +74,45 @@ for x in range(10):
     else:
         d1[x] = x
 print(d1)
+
+#18. Convert (7) to lambda function
+foo = lambda x, y: x if x < y else y
+
+#19*. Convert (8) to regular function
+def foo1(x, y, z):
+    if y < x and x > z:
+        return z
+    else:
+        return y
+
+#20. Sort lst_to_sort from min to max
+lst_to_sort = [5, 18, 1, 24, 33, 15, 13, 55]
+print(sorted(lst_to_sort))
+
+#21. Sort lst_to_sort from max to min
+print(sorted(lst_to_sort, reverse=True))
+
+#22. Use map and lambda to update the lst_to_sort by multiply each element by 2
+print(list(map(lambda x: x * 2, lst_to_sort)))
+
+#23*. Raise each list number to the corresponding number on another list:
+list_A = [2, 3, 4]
+list_B = [5, 6, 7]
+print(list(map(lambda x: x + 3, list_A)))
+
+#24. Use reduce and lambda to compute the numbers of a lst_to_sort.
+from functools import reduce
+
+
+def my_add(a, b):
+    result = a + b
+    print(f"{a} + {b} = {result}")
+    return result
+
+
+reduce(my_add, lst_to_sort)
+
+#25. Use filter and lambda to filter the number of a lst_to_sort with elem % 2 == 1.
+print(list(filter(lambda elem: (elem % 2 == 1), lst_to_sort)))"""
+
+#26. Considering the range of values: b = range(-10, 10), use the function filter to return only negative numbers.
